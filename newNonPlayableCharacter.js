6e008066cc5314ab5b0de5883cacd5f902fe5ebc
jssmith79 <i.am.hef@gmail.com>
2023-01-31 21:36:50 -0800

    final submission with corrections

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -30,21 +30,21 @@ function newNonPlayableCharacter(x, y) {
         stop()
     }
 
-    async function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
         await sleep(time)
         stop()
     }
 
-    async function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
         await sleep(time)
         stop()
     }
 
-    async function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
     
